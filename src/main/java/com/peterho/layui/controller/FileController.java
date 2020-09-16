85b8c3503d8e53fd9d09a7af8b19120c94fb57b2
PeterHo <peter45402@163.com>
2020-09-16 22:25:39 +0800

    新增读写日志controller

--- src/main/java/com/peterho/layui/controller/FileController.java
+++ src/main/java/com/peterho/layui/controller/FileController.java
@@ -1,8 +1,23 @@
 package com.peterho.layui.controller;
 
+import com.peterho.layui.service.DistrictService;
+import com.peterho.layui.service.FileService;
+import com.peterho.layui.vo.DataVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FileController {
-    
+
+    @Autowired
+    private DistrictService districtService;
+
+    @Autowired
+    private FileService fileService;
+
+    @RequestMapping("/temperature")
+    public DataVO list(Integer page, Integer limit){
+        return fileService.findData(page, limit);
+    }
 }
