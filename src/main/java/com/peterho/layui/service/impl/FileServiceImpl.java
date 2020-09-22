12c705e41ce0b6c4e96dd39f537cb576f51efdee
PeterHo <peter45402@163.com>
2020-09-22 10:49:27 +0800

    修改文件读写bug

--- src/main/java/com/peterho/layui/service/impl/FileServiceImpl.java
+++ src/main/java/com/peterho/layui/service/impl/FileServiceImpl.java
@@ -30,25 +30,8 @@ public class FileServiceImpl implements FileService {
         DataVO dataVO = new DataVO();
         dataVO.setCode(0);
         dataVO.setMessage("");
-        //写文件读写操作
-        //将其信息存入数据库
-        //并且封装成JSON返回前端
-//        IPage<Temperature> temperatureIPage = new Page<>(page, limit);
-//        IPage<Temperature> result = temperatureMapper.selectPage(temperatureIPage, null);
-//
-//        //设置当前返回数据总条数
-//        dataVO.setCount(result.getTotal());
+
         dataVO.setCount((long) 5);
-//        List<Temperature> temperatureList = result.getRecords();
-//        List<TemperatureVO> temperatureVOList = new ArrayList<>();
-//
-//        // 将数据转成VO数据
-//        for (Temperature temperature : temperatureList) {
-//            // 读取硬件部分探测到的日志数据，根据sensorid分成不同的数据集
-//            // 将数据填充到VO中
-//            TemperatureVO temperatureVO = new TemperatureVO();
-//            BeanUtils.copyProperties(temperature, temperatureVO);
-//        }
 
         // 读取文件部分
         List<TemperatureVO> temperatureVOList = new ArrayList<>();
@@ -78,26 +61,7 @@ public class FileServiceImpl implements FileService {
         DataVO dataVO = new DataVO();
         dataVO.setCode(0);
         dataVO.setMessage("200");
-        //写文件读写操作
-        //将其信息存入数据库
-        //并且封装成JSON返回前端
-//        IPage<Temperature> temperatureIPage = new Page<>(page, limit);
-//        IPage<Temperature> result = temperatureMapper.selectPage(temperatureIPage, null);
-//
-//        //设置当前返回数据总条数
-//        dataVO.setCount(result.getTotal());
         dataVO.setCount((long) 5);
-//        List<Temperature> temperatureList = result.getRecords();
-//        List<TemperatureVO> temperatureVOList = new ArrayList<>();
-//
-//        // 将数据转成VO数据
-//        for (Temperature temperature : temperatureList) {
-//            // 读取硬件部分探测到的日志数据，根据sensorid分成不同的数据集
-//            // 将数据填充到VO中
-//            TemperatureVO temperatureVO = new TemperatureVO();
-//            BeanUtils.copyProperties(temperature, temperatureVO);
-//        }
-
         // 读取文件部分
         List<TemperatureVO> temperatureVOList = new ArrayList<>();
         String filePath = "data.txt";
@@ -115,12 +79,11 @@ public class FileServiceImpl implements FileService {
             //将VO加入list
             temperatureVOList.add(temperatureVO);
 
-
             System.out.println(Double.valueOf(jo.get("temperature").toString()));
+            // 30 度以上存入数据库
             if (Double.valueOf(jo.get("temperature").toString()) >= 30.0) {
                 System.out.println("温度过高");
                 //做插入操作
-
                 Temperature temperature = new Temperature();
                 temperature.setDate(jo.get("date").toString());
                 temperature.setMsgId((Integer) jo.get("msgid"));
